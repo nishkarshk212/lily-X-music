966258f6f2ba0073414cfb9dc662bc208fa345d8
Developer <Developer@Nishkarshs-MacBook-Air.local>
2026-04-26 01:05:53 +0530

    first commit

---
+++ shreya/__init__.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2025 ShreyamousX1025
+# Licensed under the MIT License.
+# This file is part of ShreyaMusic
+
+
+import time
+import asyncio
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    format="[%(asctime)s - %(levelname)s] - %(name)s: %(message)s",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler("log.txt", maxBytes=10485760, backupCount=5),
+        logging.StreamHandler(),
+    ],
+    level=logging.INFO,
+)
+logging.getLogger("httpx").setLevel(logging.ERROR)
+logging.getLogger("ntgcalls").setLevel(logging.CRITICAL)
+logging.getLogger("pymongo").setLevel(logging.ERROR)
+logging.getLogger("pyrogram").setLevel(logging.ERROR)
+logging.getLogger("pytgcalls").setLevel(logging.ERROR)
+logger = logging.getLogger(__name__)
+
+
+__version__ = "3.0.3"
+
+from config import Config
+
+config = Config()
+config.check()
+tasks = []
+boot = time.time()
+
+from shreya.core.bot import Bot
+app = Bot()
+
+from shreya.core.dir import ensure_dirs
+ensure_dirs()
+
+from shreya.core.userbot import Userbot
+userbot = Userbot()
+
+from shreya.core.mongo import MongoDB
+db = MongoDB()
+
+from shreya.core.lang import Language
+lang = Language()
+
+from shreya.core.telegram import Telegram
+from shreya.core.youtube import YouTube
+tg = Telegram()
+yt = YouTube()
+
+from shreya.helpers import Queue, Thumbnail
+queue = Queue()
+thumb = Thumbnail()
+
+from shreya.core.calls import TgCall
+anon = TgCall()
+
+
+async def stop() -> None:
+    logger.info("Stopping...")
+    for task in tasks:
+        task.cancel()
+        try:
+            await task
+        except asyncio.exceptions.CancelledError:
+            pass
+
+    await app.exit()
+    await userbot.exit()
+    await db.close()
+    await thumb.close()
+
+    logger.info("Stopped.\n")
---
+++ shreya/plugins/start.py
@@ -0,0 +1,93 @@
+# Copyright (c) 2025 ShreyamousX1025
+# Licensed under the MIT License.
+# This file is part of ShreyaMusic
+
+import asyncio
+import random
+from pyrogram import enums, filters, types
+
+from shreya import app, config, db, lang, logger
+from shreya.helpers import buttons, utils
+
+
+@app.on_message(filters.command(["help"]) & filters.private & ~app.bl_users)
+@lang.language()
+async def _help(_, m: types.Message):
+    await m.reply_text(
+        text=m.lang["help_menu"],
+        reply_markup=buttons.help_markup(m.lang),
+        quote=True,
+    )
+
+
+@app.on_message(filters.command(["start"]))
+@lang.language()
+async def start(_, message: types.Message):
+    try:
+        if message.from_user and message.from_user.id in app.bl_users and message.from_user.id not in db.notified:
+            return await message.reply_text(message.lang["bl_user_notify"])
+
+        if len(message.command) > 1 and message.command[1] == "help":
+            return await _help(_, message)
+
+        private = message.chat.type == enums.ChatType.PRIVATE
+        _text = (
+            message.lang["start_pm"].format(message.from_user.mention if message.from_user else "User", app.mention)
+            if private
+            else message.lang["start_gp"].format(message.from_user.mention if message.from_user else "User", app.mention)
+        )
+        
+        if not config.START_IMG:
+            return await message.reply_text(_text)
+
+        _img = random.choice(config.START_IMG) if isinstance(config.START_IMG, list) else config.START_IMG
+
+        key = buttons.start_key(message.lang, private)
+        await message.reply_photo(
+            photo=_img,
+            caption=_text,
+            reply_markup=key,
+            quote=not private,
+        )
+
+        if private and message.from_user:
+            if not await db.is_user(message.from_user.id):
+                await utils.send_log(message)
+                await db.add_user(message.from_user.id)
+        elif not private:
+            if not await db.is_chat(message.chat.id):
+                await utils.send_log(message, True)
+                await db.add_chat(message.chat.id)
+    except Exception as e:
+        logger.error(f"Error in start command: {e}", exc_info=True)
+
+
+@app.on_message(filters.command(["playmode", "settings"]) & filters.group & ~app.bl_users)
+@lang.language()
+async def settings(_, message: types.Message):
+    admin_only = await db.get_play_mode(message.chat.id)
+    cmd_delete = await db.get_cmd_delete(message.chat.id)
+    skip_mode = await db.get_skip_mode(message.chat.id)
+    _language = await db.get_lang(message.chat.id)
+    await message.reply_text(
+        text=message.lang["start_settings"].format(message.chat.title),
+        reply_markup=buttons.settings_markup(
+            message.lang, admin_only, cmd_delete, skip_mode, _language, message.chat.id
+        ),
+        quote=True,
+    )
+
+
+@app.on_message(filters.new_chat_members, group=7)
+@lang.language()
+async def _new_member(_, message: types.Message):
+    if message.chat.type != enums.ChatType.SUPERGROUP:
+        return await message.chat.leave()
+
+    await asyncio.sleep(3)
+    for member in message.new_chat_members:
+        if member.id == app.id:
+            if await db.is_chat(message.chat.id):
+                return
+            await utils.send_log(message, True)
+            await db.add_chat(message.chat.id)
